6f1a5e83086f6ed62b9ef8f970f94ffd61170590
JoaoMlnC <joaomlncampos@hotmail.com>
2024-10-13 19:34:46 -0300

    Subindo implementação inicial projeto RESTFULL

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/rest/converters/NumberConverter.java
@@ -0,0 +1,23 @@
+package br.com.rest.converters;
+
+import java.util.Objects;
+
+public class NumberConverter {
+
+	
+	public static Double convertToDouble(String strNumber) {
+		if (Objects.isNull(strNumber))
+			return 0D;
+
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+		return 0D;
+	}
+
+	public static boolean isNumeric(String strNumber) {
+		if (Objects.isNull(strNumber)) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
